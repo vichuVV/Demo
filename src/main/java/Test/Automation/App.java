f8cd2a6023fd7122c75a0c9cffabd3b9ce6aa2ed
vichuVV <vishnudot35@gmail.com>
2022-01-27 20:37:24 +0530

    first commit

---
+++ src/main/java/Test/Automation/App.java
@@ -0,0 +1,13 @@
+package Test.Automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
